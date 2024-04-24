c9b999235838f394747c6adeb9b022e4b5f0dbac
MartinH <145781652+MH-WebDev@users.noreply.github.com>
2024-04-24 20:17:41 +0100

    Readme update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let category = document.querySelectorAll(".name");
 let score = document.querySelectorAll(".score");
-//let average = document.querySelectorAll(".average");
+//let average = document.querySelectorAll(".average"); Yeah, no. Not happening
 
+//Adds /100 to each result field, along with class to colour the text
 let total = document.createElement("span");
 total.textContent = " / 100";
 total.classList.add("text-light-lavender");
@@ -14,4 +15,4 @@ fetch("data.json")
       score[i].textContent = data[i].score;
       score[i].append(total.cloneNode(true));
     }
-  });
+});
